067c4534a048ee9b82a079a473f0d46664bc676d
AngelR24 <49293458+AngelR24@users.noreply.github.com>
2019-11-16 16:10:52 -0400

    Initial commit

---
+++ Converter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackConverter
+{
+    public class Converter
+    {
+        public static string ReadData()
+        {
+            return "Hola Mundo";
+        }
+    }
+}
